3b382058c4428b95f9331efba59cf78f0c0e957f
MortadhaAlaa <33283991+MortadhaAlaa@users.noreply.github.com>
2018-05-26 13:45:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os, logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+chat_ids = {]
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, 
@@ -13,17 +14,26 @@ def start(bot, update):
 المهم اَي هذا بوت تكدر تتواصل بي وي كل ادمنز قناة متت .'''.format(update.message.from_user.first_name))
 
 def message(bot, update):
-    bot.send_message(chat_id='-1001399878813', text=update.message.text)
+    m = bot.send_message(chat_id='-1001399878813', text=update.message.text)
     bot.send_message(chat_id=update.message.chat_id,
                      text='''شكرًا على ارسالك المسج حنرد عليك لمن نفتح 💙💙''')
+    chat_ids[m.message_id] = update.message.chat_id
     #bot.forward_message(chat_id='-253860529', from_chat_id=update.message.chat_id, message_id=update.message.message_id)
 
+def reply(bot, update):
+    if update.message.chat_id != '-1001399878813':
+        return
+    original_m = update.message.reply_to_message
+    original_chat = chat_ids[original_m.message_id]
+    bot.send_message(chat_id=original_chat, text=update.message.text)
+    
 def error(bot, update, error):
      logging.warning('Update "%s" caused error "%s"' % (update, error))    
 u = Updater(os.environ['TELEGRAM_TOKEN'])
 d = u.dispatcher
 d.add_handler(CommandHandler('start', start))
 d.add_handler(MessageHandler(Filters.text&Filters.private, message))
+d.add_handler(MessageHandler(Filters.text&Filters.reply, reply))
 d.add_error_handler(error)
 
 u.start_polling()
